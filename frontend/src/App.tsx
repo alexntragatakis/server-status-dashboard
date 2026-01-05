2a8bb459bea9969b578b3e15587ed0925bd92ef3
alexntragatakis <205552405+alexntragatakis@users.noreply.github.com>
2026-01-04 21:52:54 -0500

    refactor: fix status type in metrics and add test metrics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import "./App.css";
 
 export type metrics = {
-  status: "ok";
+  status: string;
   uptime_seconds: number;
   cpu_percent: number;
   memory_percent: number;
 };
 
 function App() {
+  const test_metrics = {
+    // TODO: Make logic to get actual metrics
+    status: "ok",
+    uptime_seconds: 60,
+    cpu_percent: 50,
+    memory_percent: 50,
+  };
   return <></>;
 }
 
